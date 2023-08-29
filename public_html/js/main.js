1668be16035bf514509efd52a09f6fb563743ca4
Milan Grubor <milangrubor192@gmail.com>
2023-08-29 16:22:12 +0200

    contact end

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -54,8 +54,70 @@ $(document).ready(function(){
          });
     }
     
+    //form validator (pre ovoga smo skinuli jquery validate sa neta i ukljucili ga na kontakt stranu)
     
-    
+    if ($('.contact-form').length > 0) {
+        $('.contact-form').validate({
+            highlight: function (element) {
+                $(element).addClass('is-invalid').removeClass('is-valid');
+            },
+            unhighlight: function (element) {
+                $(element).addClass('is-valid').removeClass('is-invalid');
+            },
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 3
+                },
+                email: {
+                    required: true,
+                    email: true
+                },
+                password: {
+                    required: true,
+                    minlength: 6
+                },
+                repassword: {
+                    required: true,
+                    minlength: 6,
+                    equalTo: '#password'
+                },
+                message: {
+                    required: true
+                },
+                city: {
+                    required: true
+                },
+
+                question1: {
+                    required: true
+                },
+                question2: {
+                    required: true
+                }
+            },
+
+            messages: {
+                name: {
+                    required: 'the Name field is required',
+                    minlength: 'the Name field should be min 3 charactes long'
+                }
+            },
+
+            errorElement: 'p',
+            errorPlacement: function (error, element) {
+                error.appendTo(element.closest(".form-group").find(".error-msg"));
+            }
+
+
+
+
+
+
+
+        });
+    }
+
     
     
 });
